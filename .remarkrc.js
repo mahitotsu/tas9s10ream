0d3ac4a5035da786d16cf983d090f507b88420fb
Akio Kanashiki <akio@mail.com>
2025-07-16 06:18:11 +0900

    Revert "feat: Enhance Markdown linting rules"
    This reverts commit bbecb50b0390037b84e3bedb08ab08d7e8cba36f.

--- .remarkrc.js
+++ .remarkrc.js
@@ -2,9 +2,6 @@ module.exports = {
   plugins: [
     'remark-preset-prettier',
     'remark-lint-heading-increment',
-    ['remark-lint-first-heading-level', 1],
-    'remark-lint-list-item-indent',
-    ['remark-lint-maximum-line-length', 80],
-    'remark-lint-no-missing-blank-lines'
+    ['remark-lint-first-heading-level', 1]
   ],
 };
