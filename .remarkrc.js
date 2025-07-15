bbecb50b0390037b84e3bedb08ab08d7e8cba36f
Akio Kanashiki <akio@mail.com>
2025-07-16 06:16:30 +0900

    feat: Enhance Markdown linting rules
    Add new remark-lint plugins to enforce Markdown style guide rules:
    - remark-lint-list-item-indent: Ensures correct list item indentation.
    - remark-lint-maximum-line-length: Limits line length to 80 characters.
    - remark-lint-no-missing-blank-lines: Checks for blank lines around
      headings and fences.
    
    These changes improve document consistency and quality by automating
    checks for common formatting issues.

--- .remarkrc.js
+++ .remarkrc.js
@@ -2,6 +2,9 @@ module.exports = {
   plugins: [
     'remark-preset-prettier',
     'remark-lint-heading-increment',
-    ['remark-lint-first-heading-level', 1]
+    ['remark-lint-first-heading-level', 1],
+    'remark-lint-list-item-indent',
+    ['remark-lint-maximum-line-length', 80],
+    'remark-lint-no-missing-blank-lines'
   ],
 };
